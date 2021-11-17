d472d337b34e542c490a170616a96fec9a26abcc
ByungChan Lee <lbc2415@gmail.com>
2021-11-16 23:21:00 -0600

    21.11.16 Algorithms question

--- evenOccurrence.js
+++ evenOccurrence.js
@@ -4,7 +4,7 @@ Q.
  * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
  *
- * example usage:
+ * <example usage>
  *
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
